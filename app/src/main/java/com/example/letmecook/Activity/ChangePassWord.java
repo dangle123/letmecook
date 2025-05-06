7e356d57c40f552388c8ae7158d43eaa9187a20c
dangle123 <91462553+dangle123@users.noreply.github.com>
2025-05-06 21:45:30 +0700

    commit project

--- app/src/main/java/com/example/letmecook/Activity/ChangePassWord.java
+++ app/src/main/java/com/example/letmecook/Activity/ChangePassWord.java
@@ -39,7 +39,6 @@ public class ChangePassWord extends AppCompatActivity {
                 String inputText1 = edtAgain.getText().toString().trim();
                 int length1 = inputText1.length();
 
-
                 String inputText2 = edtPass.getText().toString().trim();
                 int length2 = inputText2.length();
 
