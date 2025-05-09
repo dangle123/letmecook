ce60b9621f9e5d2390c7136753bccb8c7aa70644
dangle123 <91462553+dangle123@users.noreply.github.com>
2025-05-09 17:59:24 +0700

    commit project 5/9/2025

--- app/src/main/java/com/example/letmecook/Activity/ChangePassWord.java
+++ app/src/main/java/com/example/letmecook/Activity/ChangePassWord.java
@@ -31,8 +31,6 @@ public class ChangePassWord extends AppCompatActivity {
         edtPass = findViewById(R.id.edtChage);
         edtAgain = findViewById(R.id.edtagain);
 
-
-
         edtAgain.addTextChangedListener(new TextWatcher() {
 
             public void afterTextChanged(Editable s) {
